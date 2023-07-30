3e4e21241b694211a1c3c4d34ca181c8d0b27a82
danismz17 <128304957+danismz17@users.noreply.github.com>
2023-07-30 20:35:46 -0300

    dani v6
    agregada base de datos online
    agregado jwt
    agregado bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 // MODELS- INTERACTUAR CON LA BASE DE DATOS
 const db = require("../services/database");
 
+
 // OBTENER USUARIO
 const getUserModel = async (user_username) => {
   return await db.select().from("users").where("user_username", user_username);
@@ -22,13 +23,47 @@ const createUserModel = async (userData) => {
 };
 
 // ACTUALIZAR USUARIO
-const updateUser = async (user_username, userNewData) => {
-  return await db("users").where("user_username", user_username).update({
-    user_username: userNewData.username,
-    user_password: userNewData.userpassword,
-    user_email: userNewData.useremail,
-  });
+// const updateUser = async (user_username, userNewData) => {
+//   return await db("users").where("user_username", user_username).update({
+//     user_username: userNewData.username,
+//     user_password: userNewData.userpassword,
+//     user_email: userNewData.useremail,
+//   });
+// };
+
+const updateUser = async (username, userNewData) => {
+  try {
+    const { user_username, user_password, user_email } = userNewData;
+
+    // Verificar si el usuario existe antes de actualizar
+    const user = await knex("users").where("user_username", username).first();
+    if (!user) {
+      throw new Error("El usuario no existe.");
+    }
+
+    // Construir el objeto con los datos a actualizar
+    const updateData = {};
+    if (user_username) {
+      updateData.user_username = user_username;
+    }
+    if (user_password) {
+      updateData.user_password = user_password;
+    }
+    if (user_email) {
+      updateData.user_email = user_email;
+    }
+
+    // Realizar la actualización del usuario
+    await knex("users").where("user_username", username).update(updateData);
+  } catch (error) {
+    throw error;
+  }
 };
+
+module.exports = {
+  updateUser,
+};
+
 
 // ELIMINAR USUARIO
 const deleteUser = async (user_username) => {
--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-const express = require("express");
-const app = express();
+// Carga las variables de entorno desde el archivo .env
+require('dotenv').config();
 
+const express = require('express');
+const app = express();
+const { Pool } = require('pg');
 const routes = require("./routes");
 const users = require("./routes/users");
-require("dotenv").config();
 
 const port = process.env.APP_PORT || 3000;
+
+// Conexión a la base de datos online sin SSL y con sslmode=require
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // Usamos directamente la variable de entorno DATABASE_URL
+});
+
 app.use(express.json());
+
+// Ruta para comprobar la conexión a la base de datos
+app.get('/', async (req, res) => {
+    try {
+        // Obtener un cliente desde el pool
+        const client = await pool.connect();
+        client.release();
+        res.send("TAMOO CHELOO!");
+    } catch (err) {
+        console.error(err);
+        res.send("Error connecting to the database: " + err);
+    }
+});
 
 // HOME
 app.use("/", routes);
@@ -14,5 +35,5 @@ app.use("/", routes);
 app.use("/users", users);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}/`);
+    console.log(`Example app listening on port http://localhost:${port}/`);
 });
